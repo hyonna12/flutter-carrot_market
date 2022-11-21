d4fddf07336c57a60382a44cbedca0562016dfa1
hyonna12 <hyonna01@gmail.com>
2022-11-21 13:44:34 +0900

    home_page - product detail, item

--- lib/view/pages/main/home/home_page.dart
+++ lib/view/pages/main/home/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:carrot_market/models/product.dart';
 import 'package:carrot_market/theme.dart';
+import 'package:carrot_market/view/pages/main/home/component/product_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -8,37 +10,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: [
-            Text("좌동"),
-            SizedBox(width: 4.0),
-            Icon(
-              CupertinoIcons.chevron_down,
-              size: 15,
-            ),
-          ],
-        ),
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.search)),
-          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.list_dash)),
-          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.bell)),
-        ],
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(0.5),
-          child: Divider(
-            thickness: 1,
-            height: 1,
-            color: Colors.grey,
-          ),
-        ),
+      appBar: _buildAppBar(),
+      body: ListView.separated(
+        // 구분선 그어줌
+        itemCount: productList.length,
+        itemBuilder: (context, index) => ProductItem(productList[index]),
+        separatorBuilder: (context, index) => Divider(
+          height: 0,
+          indent: 16,
+          endIndent: 16,
+          color: Colors.grey,
+        ), // 어떤 구분선을 사용할지
       ),
-      body: Container(
-        child: Center(
-          child: Text(
-            "HomePage body",
-            style: textTheme().headline2,
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: Row(
+        children: [
+          Text("좌동"),
+          SizedBox(width: 4.0),
+          Icon(
+            CupertinoIcons.chevron_down,
+            size: 15,
           ),
+        ],
+      ),
+      actions: [
+        IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.search)),
+        IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.list_dash)),
+        IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.bell)),
+      ],
+      bottom: PreferredSize(
+        preferredSize: Size.fromHeight(0.5),
+        child: Divider(
+          thickness: 1,
+          height: 1,
+          color: Colors.grey,
         ),
       ),
     );
